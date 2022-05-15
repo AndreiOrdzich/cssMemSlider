051a1648c8ccbbbc616d50d7c77174e4db542690
AndreiOrdzich <dronpoet@mail.ru>
2022-05-15 22:34:13 +0300

    fix: fix positioning and animation behavior

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -1,12 +1,39 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+let dot1 = document.getElementsByClassName("dot-1");
+let dot2 = document.getElementsByClassName("dot-2");
+let dot3 = document.getElementsByClassName("dot-3");
+let dot4 = document.getElementsByClassName("dot-4");
 
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
+    if (n == 1) {
+        for (i = 0; i < dot1.length; i++) {
+            dot1[i].className += " active"
+        }
+    }
+    if (n == 2) {
+        for (i = 0; i < dot2.length; i++) {
+            dot2[i].className += " active"
+        }
+    }
+    if (n == 3) {
+        for (i = 0; i < dot3.length; i++) {
+            dot3[i].className += " active"
+        }
+    }
+    if (n == 4) {
+        for (i = 0; i < dot4.length; i++) {
+            dot4[i].className += " active"
+        }
+    }
 }
 
+
+
+
 function showSlides(n) {
+
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
@@ -19,11 +46,14 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
+
+
 }
+
+showSlides(slideIndex);
